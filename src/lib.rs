a5266c3165220312861a5de8d715bef04f11e23a
Peter Lyons Kehl <peter.kehl@gmail.com>
2026-04-13 18:35:39 -0700

    Types sealed

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 extern crate alloc;
 
 use alloc::{borrow::ToOwned, string::String};
+use core::marker::PhantomData;
 use serde::{Deserialize, Serialize};
 
 // Variations and params
@@ -15,7 +16,9 @@ pub mod config {
     use serde::{Deserialize, Serialize};
 
     /// Whether we the very first code block is a preamble that needs special handling.
-    #[derive(Serialize, Deserialize, Clone, Debug)]
+    ///
+    /// Intentionally NOT implementing [Clone], as we don't want user code to make copies.
+    #[derive(Serialize, Deserialize, Debug)]
     pub enum Preamble {
         /// No preamble - the very first code block is a non-Preamble block (handled by injecting
         /// any header and/or body strings if set in [crate::Config]).
@@ -46,7 +49,7 @@ pub mod config {
         use alloc::{borrow::ToOwned, string::String, vec, vec::Vec};
         use serde::{Deserialize, Serialize};
 
-        #[derive(Serialize, Deserialize, Clone, Debug)]
+        #[derive(Serialize, Deserialize, Debug)]
         #[serde(default)]
         pub struct Inserts {
             /// A list of strings to be injected after the injected
@@ -75,7 +78,7 @@ pub mod config {
         }
     }
 
-    #[derive(Serialize, Deserialize, Clone, Debug)]
+    #[derive(Serialize, Deserialize, Debug)]
     #[serde(default)]
     pub struct Headers {
         /// Prefix to be injected at the beginning of any non-preamble code block, even before an
@@ -96,9 +99,13 @@ pub mod config {
     }
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(default)]
-pub struct Config {
+/// To prevent the users on depending on pattern matching completeness etc.
+#[non_exhaustive]
+pub struct Config<S: SealedTrait> {
+    _seal: PhantomData<S>,
+
     // **Relative** path (relative to the directory of Rust source file that invoked the chain of
     // macros). Defaults to "README.md".
     pub file_path: String,
@@ -113,9 +120,11 @@ pub struct Config {
     pub ordinary_code_suffix: String,
 }
 
-impl Default for Config {
+impl<S: SealedTrait> Default for Config<S> {
     fn default() -> Self {
         Config {
+            _seal: PhantomData,
+
             file_path: "README.md".to_owned(),
 
             preamble: config::Preamble::NoPreamble,
@@ -126,18 +135,29 @@ impl Default for Config {
     }
 }
 
-impl Config {
-    /// Internal, used between crates `readme-code-extractor-core` and `readme-code-extractor` to
-    /// assure that they're of the same version.
-    #[doc(hidden)]
-    pub const fn is_exact_version(expected_version: &'static str) -> bool {
-        matches!(expected_version.as_bytes(), b"0.1.0")
-    }
+/// Internal, used between crates `readme-code-extractor-core` and `readme-code-extractor` to
+/// assure that they're of the same version.
+#[doc(hidden)]
+pub const fn is_exact_version(expected_version: &'static str) -> bool {
+    matches!(expected_version.as_bytes(), b"0.1.0")
 }
 
 #[doc(hidden)]
 const _ASSERT_VERSION: () = {
-    if !Config::is_exact_version(env!("CARGO_PKG_VERSION")) {
+    if !crate::is_exact_version(env!("CARGO_PKG_VERSION")) {
         panic!("prudent-rs/readme-code-extractor-core is of different version than expected.");
     }
 };
+
+/// Intentionally NOT public.
+struct SealedTraitParam {}
+pub trait SealedTrait {
+    #[allow(private_interfaces)]
+    fn _seal(_: &SealedTraitParam);
+}
+
+#[allow(dead_code)]
+struct SealedTraitImpl {}
+impl SealedTrait for SealedTraitImpl {
+    fn _seal(_: &SealedTraitParam) {}
+}
